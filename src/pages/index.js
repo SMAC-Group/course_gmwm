75529c6bb1a5337ad8a2e0985ab09989413b846d
Lionel Voirol <lionelvoirol@hotmail.com>
2019-12-13 01:40:39 +0100

    little bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ export default ({ data }) => {
                 <h1 className={classes.subtitle}>A Free, Interactive Course using <strong>mgcv</strong></h1>
                 <div className={classes.introduction}>
                 <p>
-                    Hello! Welcome to Generalized Additive Models in R.  This short course will teach you how to use these flexible, powerful tools to model data and solve data science problems. GAMs offer offer a middle ground between simple linear models and complex machine-learning techniques, allowing you to model and understand complex systems.
+                Hello! Welcome to Generalized Additive Models in R.  This short course will teach you how to use these flexible, powerful tools to model data and solve data science problems. GAMs offer offer a middle ground between simple linear models and complex machine-learning techniques, allowing you to model and understand complex systems.
                 </p>
                 <p>
                     To take this course, you need basic R programming skills and experience with linear regression. Now let's get started!
